0f3a534f707a73350d1612ec43c79f83772175f3
Omid Nowrouzi <onowrouzi@uco.edu>
2016-09-02 10:36:16 -0500

    Complete refactor for Maven.

---
+++ src/main/java/edu/uco/edmond/bus/tracker/indexBean.java
@@ -0,0 +1,21 @@
+
+package edu.uco.edmond.bus.tracker;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class indexBean {
+    
+    private String message = "Hello Team UCO!";
+    
+    public String getMessage(){
+        return message;
+    }
+    
+    public void setMessage(String message){
+        this.message = message;
+    }
+    
+}
