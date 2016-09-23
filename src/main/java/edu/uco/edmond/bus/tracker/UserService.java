9fa8b9337db6ad369988431d308d96c56209dbcc
Omid Nowrouzi <onowrouzi@outlook.com>
2016-09-22 20:10:00 -0500

    Changes by Drew on UserService

--- src/main/java/edu/uco/edmond/bus/tracker/UserService.java
+++ src/main/java/edu/uco/edmond/bus/tracker/UserService.java
@@ -3,12 +3,16 @@ package edu.uco.edmond.bus.tracker;
 
 import com.google.gson.Gson;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,83 +21,45 @@ import javax.ws.rs.core.MediaType;
 @Path("userservice")
 public class UserService {
     
-    //public User test = new User("test", "test", "test");
+    final public String TABLENAME = "tblusers";
     Gson gson = new Gson();
-    DBConnect db;
     Connection database;
-    boolean upToDate;
-    List<User> users; //may change to hashset to make sure a user is not duplicated in db.
+    List<User> users;
     
-    public UserService()
+    public UserService() throws SQLException
     {
-        db = new DBConnect();
-        this.database = db.getDatabase();
-        this.upToDate = false;
+        this.database = new DBConnect().getDatabase();
         this.users = new ArrayList<>();
+        getAllUsers();
     }
     
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("users")
-    public String getUsers() throws SQLException {
-        
+    private void getAllUsers() throws SQLException
+    {
         Statement stmt = database.createStatement();
         
         ResultSet rs = stmt.executeQuery("SELECT * FROM tblusers");
-        
+
         while(rs.next()){
             User user = new User(rs.getInt("id"), rs.getString("username"), rs.getString("password"), rs.getString("usertype"));
             users.add(user);
         }
-        
-        upToDate = true;
-        
-        return gson.toJson(users);
     }
     
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("users/{id}")
-    public String getUser(@PathParam("id") int id) throws SQLException {
-        if(!upToDate)
-            getUsers();
-        
+    public User find(int id)
+    {
         for(User user : users)
         {
             if(user.getId() == id)
             {
-                return gson.toJson(user); //user found
+                return user; //user found
             }
         }
         
-        //no user found
-        return gson.toJson(null);
+        return null;
     }
-    
-    public User administratorLogin(String username, String password) throws SQLException
-    {
-        User user = login(username, password);
         
-        if(user != null)
-            if("Administrator".equals(user.getType()))
-                return user; //admin found
-            else
-                return null; //user is only allowed on mobile --send message to client code
-        
-        return null; //user not found
-    }
-    
-    public User clientLogin(String username, String password) throws SQLException
+    public User find(String username, String password)
     {
-        //admin should be able to login to both mobile and web systems
-        return login(username, password);
-    }
-    
-    private User login(String username, String password) throws SQLException
-    {
-        if(!upToDate)
-            getUsers();
-        
         for(User user : users)
         {
             if(user.getUsername().equals(username))
@@ -109,29 +75,98 @@ public class UserService {
         return null;
     }
     
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("users")
+    public String getUsers(){
+        if(users.isEmpty())
+            return gson.toJson(null); // no users in system
+        
+        return gson.toJson(users);
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("users/{id}")
+    public String getUser(@PathParam("id") int id) throws SQLException {
+        return gson.toJson(find(id));
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("users/administrator/{username}/{password}") //talk about using optional params ?isadmin=true
+    public String administratorLogin(@PathParam("username") String username, @PathParam("password") String password) throws SQLException
+    {
+        User user = find(username, password);
+        
+        if(user != null)
+            if("Administrator".equals(user.getType()))
+                return gson.toJson(user); //admin found
+            else
+                return gson.toJson(null); //user is only allowed on mobile --send message to client code
+        
+        return gson.toJson(null); //user not found
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("users/{username}/{password}")//talk about using optional params ?isadmin=false
+    public String clientLogin(@PathParam("username") String username, @PathParam("password") String password) throws SQLException
+    {
+        //admin should be able to login to both mobile and web systems
+        return gson.toJson(find(username, password));
+    }
+    
     public void logout()
     {
         
     }
     
-    public User register(String username, String password)
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public User register(@FormParam("username") String username, @FormParam("password") String password, @FormParam("type") String type)
     {
-        //make SQL call and change the users list in code (than we can remove the booleans) or just keep the booleans and just make the appropriate SQL calls. We will discuss
-        upToDate = false;
-        return null;
+        User tempUser = find(username, password);
+        
+        if(tempUser != null)
+            return null; //send error message on client --user exists
+        
+        try{
+            PreparedStatement stmt = database.prepareStatement("INSERT INTO tblusers VALUES(?,?,?)");
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            stmt.setString(3, type);
+
+            int count = stmt.executeUpdate(); //?test executeupdate()???
+
+            //get id of new user
+            PreparedStatement stmt2 = database.prepareStatement("SELECT * FROM tblusers WHERE username=? AND password=?");
+            stmt2.setString(1, username);
+            stmt2.setString(2, password);
+
+            ResultSet rs = stmt2.executeQuery();
+
+            int id = rs.getInt("id");
+            User user = new User(id,username,password,type);
+            users.add(user);
+
+            return user;
+        }catch(SQLException s){
+            return null; //SQL failed
+        }
     }
     
-    public User edit(String oldUsername, String oldPassword, String newUsername, String newPassword)
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public User edit(@FormParam("username") String username, @FormParam("oldpassword")String oldPassword, @FormParam("newpassword") String newPassword)
     {
-        //make SQL call and change the users list in code (than we can remove the booleans) or just keep the booleans and just make the appropriate SQL calls. We will discuss
-        upToDate = false;
         return null;
     }
     
     public User delete(String username, String password)
     {
-        //make SQL call and change the users list in code (than we can remove the booleans) or just keep the booleans and just make the appropriate SQL calls. We will discuss
-        upToDate = false;
         return null;
     }
 }
