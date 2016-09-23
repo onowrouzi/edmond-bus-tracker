4621777cc8b3ae16b1ff10d85f930b3fb80a96f1
Omid Nowrouzi <onowrouzi@outlook.com>
2016-09-22 23:14:33 -0500

    Added Bootsfaces and test stylings

--- src/main/java/edu/uco/edmond/bus/tracker/indexBean.java
+++ src/main/java/edu/uco/edmond/bus/tracker/indexBean.java
@@ -1,4 +1,4 @@
-
+//test Demechko branch
 package edu.uco.edmond.bus.tracker;
 
 import javax.faces.bean.ManagedBean;
@@ -7,10 +7,11 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean
 @RequestScoped
 public class indexBean {
-    
+    DBConnect db = new DBConnect();
     private String message = "Hello Team UCO!";
     
     public String getMessage(){
+        //UserService userService = new UserService(db.getDatabase());
         return message;
     }
     
