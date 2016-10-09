db3e12935c89a94d465446614554a9fc6184239b
Logan Huskins <loganghuskins@gmail.com>
2016-10-09 16:04:21 -0500

    Update BusStop for new DB

--- src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -29,7 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(edu.uco.edmond.bus.tracker.Services.BusService.class);
+        resources.add(edu.uco.edmond.bus.tracker.Services.BusStopService.class);
         resources.add(edu.uco.edmond.bus.tracker.Services.FavoriteService.class);
         resources.add(edu.uco.edmond.bus.tracker.Services.UserService.class);
     }
