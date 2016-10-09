e7c9cf3e0f14e0d49416e4465b2a8165e68c6cee
Omid Nowrouzi <onowrouzi@outlook.com>
2016-10-09 18:28:53 -0500

    Added rough bus service

---
+++ src/main/java/edu/uco/edmond/bus/tracker/Services/BusService.java
@@ -0,0 +1,187 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uco.edmond.bus.tracker.Services;
+
+import edu.uco.edmond.bus.tracker.Dtos.Bus;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author omidnowrouzi
+ */
+@Path("busservice")
+public class BusService extends Service {
+     private List<Bus> buses;
+    
+    public BusService() throws SQLException
+    {
+        this.buses = new ArrayList<>();
+        getAllBuses();
+    }
+    
+    public List<Bus> buses()
+    {
+        return buses;
+    }
+    
+    private void getAllBuses() throws SQLException
+    {
+        
+        Statement stmt = getDatabase().createStatement();
+        
+        ResultSet rs = stmt.executeQuery("SELECT * FROM tblbus");
+
+        while(rs.next()){
+            Bus bus = new Bus(rs.getInt("id"), rs.getString("name"), rs.getString("driver"), rs.getString("route"));
+            buses.add(bus);
+        }
+        
+        stmt.close();
+    }
+    
+    public Bus find(int id)
+    {
+        for(Bus bus : buses)
+        {
+            if(bus.getId() == id)
+            {
+                return bus; //bus found
+            }
+        }
+            
+        return null;
+    }
+    
+    public Bus find(String query)
+    {
+        for(Bus bus : buses)
+        {
+            if(bus.getName().equals(query) || bus.getDriver().equals(query) || bus.getRoute().equals(query))
+            {
+                return bus; //bus found
+            }
+        }
+        
+        //no stop found
+        return null;
+    }   
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("buses")
+    public String getBusess()
+    {
+        if(buses.isEmpty())
+            return getGson().toJson("No buses currently registered."); // no buses in system
+        
+        return getGson().toJson(buses);
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("buses/byid/{id}")
+    public String getBus(@PathParam("id") int id) throws SQLException {
+        return getGson().toJson(find(id));
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("buses/{query}")
+    public String getBus(@PathParam("query") String query) throws SQLException {
+        return getGson().toJson(find(query));
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("buses/edit/{oldName}/{newName}/{oldDriver}/{newDriver}/{oldRoute}/{newRoute}")
+    public Bus edit(@PathParam("oldName") String oldName, @PathParam("newName") String newName, 
+            @PathParam("oldDriver") String oldDriver, @PathParam("newDriver") String newDriver,
+                @PathParam("oldRoute") String oldRoute, @PathParam("newRoute") String newRoute)
+    {
+        return null;
+    }
+   
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("buses/create/{name}/{driver}/{route}")
+    public String create(@PathParam("name") String name, @PathParam("driver") String driver, @PathParam("route") String route)
+    {
+        Bus bus = find(name);
+        
+        if(bus != null)
+            return getGson().toJson(null); //send error message on client --bus exists
+        
+        try{
+            PreparedStatement stmt = getDatabase().prepareStatement("INSERT INTO tblbus (name, driver, route) VALUES(?,?,?)");
+            stmt.setString(1, name);
+            stmt.setString(2, driver);
+            stmt.setString(3, route);
+
+            int count = stmt.executeUpdate();
+            
+            stmt.close();
+
+            //get id of new stop
+            PreparedStatement stmt2 = getDatabase().prepareStatement("SELECT id FROM tblbus WHERE name=?");
+            stmt2.setString(1, name);
+
+            ResultSet rs = stmt2.executeQuery();
+            
+            rs.first();
+
+            int id = rs.getInt("id");
+            bus = new Bus(id,name,driver,route);
+            buses.add(bus);  
+            
+            stmt2.close();
+            
+        }catch(SQLException s){
+            return getGson().toJson(s.toString()); //SQL failed
+        }
+        
+        return getGson().toJson(bus);
+    }
+    
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("buses/delete/{name}")
+    public String delete(@PathParam("name") String name)
+    {
+        Bus bus = find(name);
+        
+        if(bus == null)
+            return getGson().toJson(null); //send error message on client --bus does not exist
+        
+        try{
+            PreparedStatement stmt = getDatabase().prepareStatement("DELETE FROM tblbus WHERE id=?");
+            stmt.setInt(1, bus.getId());
+
+            int count = stmt.executeUpdate();
+            
+            stmt.close();
+            
+        }catch(SQLException s){
+            return getGson().toJson(s.toString());
+        }
+        
+        buses.remove(bus);
+        
+        return getGson().toJson(bus);
+    }
+}
