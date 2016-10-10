e801c892a2bf8340222edfcfd42e75250b0144a2
Logan Huskins <loganghuskins@gmail.com>
2016-10-10 06:45:16 -0500

    wip - adding new stop creation

--- src/main/java/edu/uco/edmond/bus/tracker/CreateRouteView.java
+++ src/main/java/edu/uco/edmond/bus/tracker/CreateRouteView.java
@@ -28,130 +28,130 @@ import org.primefaces.model.map.Marker;
 @ViewScoped
 public class CreateRouteView implements Serializable {
     
-    private MapModel draggableModel;
-    private Marker marker;
-    
-    private Route route = new Route();
-    
-    private final double defaultLat = 35.6526783;
-    private final double defaultLng = -97.4781833;
-    private final String defaultStopName = "Route";
-        
-    private int currentRouteOrderNumber = 1;
-    
-    public String mapKey = "https://maps.google.com/maps/api/js?key=" + System.getenv("MAP_API");
-        
-    HttpURLConnection connection = null;
-    
-    // temp since we don't have users implemented yet
-    private ArrayList<User> possibleDrivers = new ArrayList<>();
-        
-    @PostConstruct
-    public void init() {
-        this.draggableModel = new DefaultMapModel();
-    }
-      
-    public MapModel getDraggableModel() {
-        return draggableModel;
-    }
-      
-    public void onMarkerDrag(MarkerDragEvent event) {
-        marker = event.getMarker();
-        LatLng coords = new LatLng(marker.getLatlng().getLat(), marker.getLatlng().getLng());
-        
-        for (RouteStop r : this.route.getRoutes()) {
-            if (r.getIdentifier().equals(marker.getTitle())) {
-                r.setLocation(coords);
-            }
-        }
-        this.showMessage("Marker Moved","Lat:" + marker.getLatlng().getLat() + ", Lng:" + marker.getLatlng().getLng());
-    }
-    
-    public void addRoute() {
-        String tempUuid = UUID.randomUUID().toString();
-        RouteStop routeStop = new RouteStop();
-        LatLng coords = new LatLng(defaultLat, defaultLng);
-        this.addMarker(coords, tempUuid);
-        routeStop.setStopOnRoute(currentRouteOrderNumber++);
-        routeStop.setStopName(defaultStopName);
-        routeStop.setLocation(coords);
-        routeStop.setIdentifier(tempUuid);
-        this.route.addRoute(routeStop);
-    }
-    
-    public void delete() {
-        // delete logic
-    }
-    
-    public void save() {
-        
-        for (RouteStop r : this.route.getRoutes()) {
-            System.out.println(r.getStopName());
-            DataOutputStream wr = null;
-            try {
-                String routeStopName = r.getStopName();
-                String lat = String.valueOf(r.getLocation().getLat());
-                String lng = String.valueOf(r.getLocation().getLng());
-                String route = "https://uco-edmond-bus.herokuapp.com/api/busservice/stops/create/" + routeStopName + "/" + lat + "/" + lng;
-                System.out.println(route);
-                URL url = new URL(route);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-                connection.setRequestProperty("User-Agent", "Mozilla/5.0");
-                
-                int responseCode = connection.getResponseCode();
-		System.out.println("\nSending 'GET' request to URL : " + url);
-		System.out.println("Response Code : " + responseCode);
-
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(connection.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
-
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
-
-		//print result
-		System.out.println(response.toString());
- 
-            } catch (IOException ex) {
-                Logger.getLogger(CreateRouteView.class.getName()).log(Level.SEVERE, null, ex);
-            } finally {
-
-            }
-        }
-    }
-    
-    public Route getRoute() {
-        return route;
-    }
-
-    public void setRoute(Route route) {
-        this.route = route;
-    }
-    
-    private void addMarker(LatLng coords, String name) {
-        Marker newMarker = new Marker(coords, name);
-        newMarker.setDraggable(true);
-        this.draggableModel.addOverlay(newMarker);
-    }
-    
-    private void showMessage(String heading, String message) {
-        FacesContext context = FacesContext.getCurrentInstance();
-        context.addMessage(null, new FacesMessage(heading, message));
-    }
-
-    public ArrayList<User> getPossibleDrivers() {
-        return possibleDrivers;
-    }
-
-    public void setPossibleDrivers(ArrayList<User> possibleDrivers) {
-        this.possibleDrivers = possibleDrivers;
-    }
-    
-    public String getMapKey() {
-        return this.mapKey;
-    }
+//    private MapModel draggableModel;
+//    private Marker marker;
+//    
+//    private Route route = new Route();
+//    
+//    private final double defaultLat = 35.6526783;
+//    private final double defaultLng = -97.4781833;
+//    private final String defaultStopName = "Route";
+//        
+//    private int currentRouteOrderNumber = 1;
+//    
+//    public String mapKey = "https://maps.google.com/maps/api/js?key=" + System.getenv("MAP_API");
+//        
+//    HttpURLConnection connection = null;
+//    
+//    // temp since we don't have users implemented yet
+//    private ArrayList<User> possibleDrivers = new ArrayList<>();
+//        
+//    @PostConstruct
+//    public void init() {
+//        this.draggableModel = new DefaultMapModel();
+//    }
+//      
+//    public MapModel getDraggableModel() {
+//        return draggableModel;
+//    }
+//      
+//    public void onMarkerDrag(MarkerDragEvent event) {
+//        marker = event.getMarker();
+//        LatLng coords = new LatLng(marker.getLatlng().getLat(), marker.getLatlng().getLng());
+//        
+//        for (RouteStop r : this.route.getRoutes()) {
+//            if (r.getIdentifier().equals(marker.getTitle())) {
+//                r.setLocation(coords);
+//            }
+//        }
+//        this.showMessage("Marker Moved","Lat:" + marker.getLatlng().getLat() + ", Lng:" + marker.getLatlng().getLng());
+//    }
+//    
+//    public void addRoute() {
+//        String tempUuid = UUID.randomUUID().toString();
+//        RouteStop routeStop = new RouteStop();
+//        LatLng coords = new LatLng(defaultLat, defaultLng);
+//        this.addMarker(coords, tempUuid);
+//        routeStop.setStopOnRoute(currentRouteOrderNumber++);
+//        routeStop.setStopName(defaultStopName);
+//        routeStop.setLocation(coords);
+//        routeStop.setIdentifier(tempUuid);
+//        this.route.addRoute(routeStop);
+//    }
+//    
+//    public void delete() {
+//        // delete logic
+//    }
+//    
+//    public void save() {
+//        
+//        for (RouteStop r : this.route.getRoutes()) {
+//            System.out.println(r.getStopName());
+//            DataOutputStream wr = null;
+//            try {
+//                String routeStopName = r.getStopName();
+//                String lat = String.valueOf(r.getLocation().getLat());
+//                String lng = String.valueOf(r.getLocation().getLng());
+//                String route = "https://uco-edmond-bus.herokuapp.com/api/busservice/stops/create/" + routeStopName + "/" + lat + "/" + lng;
+//                System.out.println(route);
+//                URL url = new URL(route);
+//                connection = (HttpURLConnection) url.openConnection();
+//                connection.setRequestMethod("GET");
+//                connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+//                
+//                int responseCode = connection.getResponseCode();
+//		System.out.println("\nSending 'GET' request to URL : " + url);
+//		System.out.println("Response Code : " + responseCode);
+//
+//		BufferedReader in = new BufferedReader(
+//		        new InputStreamReader(connection.getInputStream()));
+//		String inputLine;
+//		StringBuffer response = new StringBuffer();
+//
+//		while ((inputLine = in.readLine()) != null) {
+//			response.append(inputLine);
+//		}
+//		in.close();
+//
+//		//print result
+//		System.out.println(response.toString());
+// 
+//            } catch (IOException ex) {
+//                Logger.getLogger(CreateRouteView.class.getName()).log(Level.SEVERE, null, ex);
+//            } finally {
+//
+//            }
+//        }
+//    }
+//    
+//    public Route getRoute() {
+//        return route;
+//    }
+//
+//    public void setRoute(Route route) {
+//        this.route = route;
+//    }
+//    
+//    private void addMarker(LatLng coords, String name) {
+//        Marker newMarker = new Marker(coords, name);
+//        newMarker.setDraggable(true);
+//        this.draggableModel.addOverlay(newMarker);
+//    }
+//    
+//    private void showMessage(String heading, String message) {
+//        FacesContext context = FacesContext.getCurrentInstance();
+//        context.addMessage(null, new FacesMessage(heading, message));
+//    }
+//
+//    public ArrayList<User> getPossibleDrivers() {
+//        return possibleDrivers;
+//    }
+//
+//    public void setPossibleDrivers(ArrayList<User> possibleDrivers) {
+//        this.possibleDrivers = possibleDrivers;
+//    }
+//    
+//    public String getMapKey() {
+//        return this.mapKey;
+//    }
 }
