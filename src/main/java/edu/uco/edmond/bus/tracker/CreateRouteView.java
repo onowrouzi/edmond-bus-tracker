8634f689757a9a95f29857cd785020ce18cf2f38
Logan Huskins <logan@clevyr.com>
2016-10-20 18:13:10 -0500

    wip adding routes

--- src/main/java/edu/uco/edmond/bus/tracker/CreateRouteView.java
+++ src/main/java/edu/uco/edmond/bus/tracker/CreateRouteView.java
@@ -19,6 +19,9 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
   
 import org.primefaces.event.map.MarkerDragEvent;
+import org.primefaces.json.JSONArray;
+import org.primefaces.json.JSONException;
+import org.primefaces.json.JSONObject;
 import org.primefaces.model.map.DefaultMapModel;
 import org.primefaces.model.map.LatLng;
 import org.primefaces.model.map.MapModel;
@@ -27,122 +30,93 @@ import org.primefaces.model.map.Marker;
 @ManagedBean
 @ViewScoped
 public class CreateRouteView implements Serializable {
-    
-    /**
-     * THIS WILL NOT WORK SINCE WE DID THE DATABASE STUFF
-     */
-    
+
     private MapModel draggableModel;
     private Marker marker;
     
     private Route route = new Route();
-    
-    private final double defaultLat = 35.6526783;
-    private final double defaultLng = -97.4781833;
-    private final String defaultStopName = "Route";
+    private ArrayList<RouteStop> stops = new ArrayList<>();
+    private ArrayList<RouteStop> stopOnRoute = new ArrayList<>();
+    private int currentStopOnRoute = 1;
         
     private int currentRouteOrderNumber = 1;
     
     public String mapKey = "https://maps.google.com/maps/api/js?key=" + System.getenv("MAP_API");
         
     HttpURLConnection connection = null;
-    
-    // temp since we don't have users implemented yet
-    private ArrayList<User> possibleDrivers = new ArrayList<>();
-        
+            
     @PostConstruct
     public void init() {
-        this.draggableModel = new DefaultMapModel();
-    }
-      
-    public MapModel getDraggableModel() {
-        return draggableModel;
-    }
-      
-    public void onMarkerDrag(MarkerDragEvent event) {
-        marker = event.getMarker();
-        LatLng coords = new LatLng(marker.getLatlng().getLat(), marker.getLatlng().getLng());
-
-        this.showMessage("Marker Moved","Lat:" + marker.getLatlng().getLat() + ", Lng:" + marker.getLatlng().getLng());
-    }
-    
-    public void addRoute() {
-
-    }
-    
-    public void delete() {
-        // delete logic
-    }
-    
-    public void save() {
-        
-        for (RouteStop r : this.route.getRoutes()) {
-            System.out.println(r.getStopName());
-            DataOutputStream wr = null;
+        try {
+            String url = "https://uco-edmond-bus.herokuapp.com/api/busstopservice/stops";
+            
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            
+            // optional default is GET
+            con.setRequestMethod("GET");
+            
+            //add request header
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            
+            int responseCode = con.getResponseCode();
+            
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            
+            //print result
+            JSONArray jsonarray;
             try {
-                String routeStopName = r.getStopName();
-                String lat = String.valueOf(r.getLocation().getLat());
-                String lng = String.valueOf(r.getLocation().getLng());
-                String route = "https://uco-edmond-bus.herokuapp.com/api/busservice/stops/create/" + routeStopName + "/" + lat + "/" + lng;
-                System.out.println(route);
-                URL url = new URL(route);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-                connection.setRequestProperty("User-Agent", "Mozilla/5.0");
-                
-                int responseCode = connection.getResponseCode();
-		System.out.println("\nSending 'GET' request to URL : " + url);
-		System.out.println("Response Code : " + responseCode);
+                jsonarray = new JSONArray(response.toString());
+                for (int i = 0; i < jsonarray.length(); i++) {
+                JSONObject jsonobject;
+                try {
+                    jsonobject = jsonarray.getJSONObject(i);
+                    String id = jsonobject.getString("id");
+                    String name = java.net.URLDecoder.decode(jsonobject.getString("name"), "UTF-8");
+                    Double lat = jsonobject.getDouble("latitude");
+                    Double lng = jsonobject.getDouble("longitude");
+                    RouteStop temp = new RouteStop();
+                    temp.setStopName(name);
+                    temp.setLocation(new LatLng(lat, lng));
+                    stops.add(temp);
+                } catch (JSONException ex) {
+                    Logger.getLogger(RouteManagement.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            } catch (JSONException ex) {
+                Logger.getLogger(RouteManagement.class.getName()).log(Level.SEVERE, null, ex);
+            }
 
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(connection.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
-
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
-
-		//print result
-		System.out.println(response.toString());
- 
-            } catch (IOException ex) {
-                Logger.getLogger(CreateRouteView.class.getName()).log(Level.SEVERE, null, ex);
-            } finally {
-
+        } catch (IOException ex) {
+            Logger.getLogger(RouteManagement.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public ArrayList<RouteStop> getStops() {
+        return this.stops;
+    }
+    
+    public ArrayList<RouteStop> getStopsOnRoute() {
+        return this.stopOnRoute;
+    }
+    
+    public void addToRoute(String name) {
+        for (RouteStop r : this.stops) {
+            if (r.getStopName().equals(name)) {
+                this.stopOnRoute.add(r);
             }
         }
     }
     
-    public Route getRoute() {
-        return route;
-    }
-
-    public void setRoute(Route route) {
-        this.route = route;
-    }
-    
-    private void addMarker(LatLng coords, String name) {
-        Marker newMarker = new Marker(coords, name);
-        newMarker.setDraggable(true);
-        this.draggableModel.addOverlay(newMarker);
-    }
-    
-    private void showMessage(String heading, String message) {
-        FacesContext context = FacesContext.getCurrentInstance();
-        context.addMessage(null, new FacesMessage(heading, message));
-    }
-
-    public ArrayList<User> getPossibleDrivers() {
-        return possibleDrivers;
-    }
-
-    public void setPossibleDrivers(ArrayList<User> possibleDrivers) {
-        this.possibleDrivers = possibleDrivers;
-    }
-    
-    public String getMapKey() {
-        return this.mapKey;
+    public void removeFromRoute(String name) {
+        System.out.println(name);
     }
 }
--- src/main/java/edu/uco/edmond/bus/tracker/RouteStop.java
+++ src/main/java/edu/uco/edmond/bus/tracker/RouteStop.java
@@ -3,10 +3,11 @@ package edu.uco.edmond.bus.tracker;
 import org.primefaces.model.map.LatLng;
 
 public class RouteStop {
-    
+
     private String stopName;
     private LatLng location;
-    
+    private int stopOnRoute = 0;
+
     public RouteStop() {
     }
 
@@ -25,12 +26,20 @@ public class RouteStop {
     public void setLocation(LatLng location) {
         this.location = location;
     }
-    
+
     public double getStopLat() {
         return this.location.getLat();
     }
-    
-     public double getStopLng() {
+
+    public double getStopLng() {
         return this.location.getLng();
+    }
+    
+    public int getStopOnRoute() {
+        return stopOnRoute;
+    }
+
+    public void setStopOnRoute(int stopOnRoute) {
+        this.stopOnRoute = stopOnRoute;
     }
 }
