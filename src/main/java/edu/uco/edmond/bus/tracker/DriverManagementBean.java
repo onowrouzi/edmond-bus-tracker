a4fb5cac42c8e434d1151e2f157f42451016b017
kathleencao <thinicao@hotmail.com>
2016-11-08 15:10:25 -0600

    Created Edit Driver
    Created Edit Driver file

--- src/main/java/edu/uco/edmond/bus/tracker/DriverManagementBean.java
+++ src/main/java/edu/uco/edmond/bus/tracker/DriverManagementBean.java
@@ -94,6 +94,47 @@ public class DriverManagementBean implements Serializable {
         }
     }
     
+    public String editDriver(String username, String firstName, String lastName, String email, String password, String confirmPassword) throws Exception{
+        if (!password.equals(confirmPassword) || (password.isEmpty() && confirmPassword.isEmpty())) {
+            return "Passwords must match!";
+        }
+        
+        try {
+            String url = ENV + "/edit/" + username + "/" + firstName + "/" + lastName + "/" + email + "/"+ password + "/" + confirmPassword + "/";
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            
+            // optional default is GET
+            con.setRequestMethod("GET");
+            
+            //add request header
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            
+            int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'GET' request to URL : " + url);
+            System.out.println("Response Code : " + responseCode);
+            
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            con.disconnect();
+            
+        } catch (Exception ex) {
+            Logger.getLogger(UserManagementBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        context.redirect("driverManagement.xhtml");
+        
+        return "Driver was edited!";
+    }
+
+    
     public String getUsername() {
         return username;
     }
